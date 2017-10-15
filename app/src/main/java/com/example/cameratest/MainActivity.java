08ea522aa946996a2da5c3d3886e6e1745a9c398
Minghao Shan <minghao8shan@gmail.com>
2017-10-15 01:07:16 -0500

    commit

--- app/src/main/java/com/example/cameratest/MainActivity.java
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -1,49 +1,57 @@
 package com.example.cameratest;
 
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
-import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    Button button_Category;
+    static final int CAM_REQUEST = 1;
+    public static String imageName;
     String filename;
     @Override
     protected void onCreate(Bundle savedInstanceBundle){
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button_Capture);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                filename = "fname_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+                Uri imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), filename));
+                intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, 0);
+            }
+        });
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        filename = "fname_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
-        Uri imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), filename));
-        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
-        startActivityForResult(intent, 0);
+
+    }
+
+    private File getFlie(){
+        File folder = new File("sdcard/camera_app");
+
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+        imageName = String.valueOf(System.currentTimeMillis())+".jpg";
+        File image_file = new File(folder, imageName);
+        return image_file;
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == 0 && resultCode == RESULT_OK){
-            Intent intent = new Intent(this, second.class);
+            Intent intent = new Intent(this, chooseCategory.class);
             intent.putExtra("FILENAME", filename);
             startActivity(intent);
         }
