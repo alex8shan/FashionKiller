d17a11318a78a67948845078846f79b86b73d47c
Pasindu Tennakoon <pasindutennakoon@test-129099.dhcp.missouri.edu>
2017-10-15 09:17:22 -0500

    This works?

--- app/src/main/java/com/example/cameratest/second.java
+++ app/src/main/java/com/example/cameratest/second.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import android.graphics.Matrix;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +12,22 @@ import android.util.Log;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import static org.opencv.core.Core.bitwise_and;
 import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
+import static org.opencv.imgproc.Imgproc.contourArea;
+import static org.opencv.imgproc.Imgproc.drawContours;
 
 
 public class second extends AppCompatActivity {
@@ -42,11 +52,12 @@ public class second extends AppCompatActivity {
         if(imgFile.exists()){
 
             Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-
+            myBitmap = RotateBitmap(myBitmap, 90);
             ImageView myImage = (ImageView) findViewById(R.id.imageView);
 
             myImage.setImageBitmap(myBitmap);
-            //ProcessImage(myBitmap);
+
+            ProcessImage(myBitmap);
 
         }
         else {
@@ -55,14 +66,41 @@ public class second extends AppCompatActivity {
 
 
     }
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
     public void ProcessImage(Bitmap b){
 
         Mat tmp = new Mat (b.getWidth(), b.getHeight(), CvType.CV_8UC1);
         Utils.bitmapToMat(b, tmp);
+        Mat original = tmp.clone();
         Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_RGB2GRAY);
-//there could be some processing
+        Imgproc.GaussianBlur(tmp, tmp, new Size(5, 5), 0);
         Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_GRAY2RGB, 4);
-        Utils.matToBitmap(tmp, b);
+        Mat src = new Mat();
+        Imgproc.Canny(tmp, src, 20, 120);
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(5, 5));
+        Imgproc.dilate(src, src, kernel);
+        Imgproc.dilate(src, src, kernel);
+        Imgproc.dilate(src, src, kernel);
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(src.clone(), contours, new Mat(), Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_SIMPLE);
+        int maxi = 0;
+        for (int i = 0; i < contours.size(); i++){
+            if (contourArea(contours.get(i)) > contourArea(contours.get(maxi))) {
+                maxi = i;
+            }
+        }
+        Mat mask = new Mat( new Size( src.cols(), src.rows() ), CvType.CV_8UC1 );
+        mask.setTo( new Scalar( 0.0 ) );
+        drawContours(mask, contours, maxi, new Scalar(255), -1);
+        original.copyTo(src, mask);
+
+        Utils.matToBitmap(src, b);
         ImageView myImage = (ImageView) findViewById(R.id.imageView);
         myImage.setImageBitmap(b);
     }
