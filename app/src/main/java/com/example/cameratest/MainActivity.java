91f531b526b987f4f406fe0f801c795821df03ac
hailulai <hailu0731@gmail.com>
2017-10-14 22:06:38 -0500

    swipe log

--- app/src/main/java/com/example/cameratest/MainActivity.java
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -7,15 +7,22 @@ import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.view.View.OnTouchListener;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -49,6 +56,23 @@ public class MainActivity extends Activity {
                 startActivityForResult(camera_intent, CAM_REQUEST);
 
             }
+        });
+
+
+        ConstraintLayout rl = (ConstraintLayout) findViewById(R.id.something);
+
+        rl.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                Log.i("move", "something");
+                imageView.setImageResource(R.drawable.sameple_4);
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    Toast.makeText(getApplicationContext(), "Swiped Down", Toast.LENGTH_SHORT);
+                    return true;
+                }
+                return false;
+            }
+
         });
     }
 
